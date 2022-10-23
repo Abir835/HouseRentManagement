319b0f0726647a1f3d79c1128e18b3dc1e1774e8
abir <abir.hasan@divine-it.bet>
2022-10-23 20:43:56 +0600

    APP<Custom Addons> add odoo project in my git

---
+++ custom_addons/om_hospital/__manifest__.py
@@ -0,0 +1,20 @@
+{
+    'name': 'Hospital Management',
+    'version': '1.0.0',
+    'category': 'Hospital Management',
+    'description': """Hospital Management System""",
+    'summary': 'Hospital Management System',
+    'depends': [],
+    'data': [
+        'views/menu.xml',
+        'views/patient_view.xml',
+    ],
+    'demo': [],
+    'sequence': -100,
+    'author': 'Odoo Mates',
+    'auto_install': True,
+    'application': True,
+    'installable': True,
+    'license': 'LGPL-3',
+
+}
