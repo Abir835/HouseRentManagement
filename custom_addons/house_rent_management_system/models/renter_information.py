744377fc0426f630377b5f3614158c13b1ab81cb
abir <abir.hasan@divine-it.bet>
2022-10-27 01:19:54 +0600

    APP<House Rent Management System> ADD:: Birthday calculation age on change

--- custom_addons/house_rent_management_system/models/renter_information.py
+++ custom_addons/house_rent_management_system/models/renter_information.py
@@ -1,4 +1,5 @@
 from datetime import datetime
+import datetime
 from odoo import api, fields, models
 
 
@@ -7,12 +8,22 @@ class RenterInformation(models.Model):
     _description = 'Renter Information'
 
     name = fields.Char(string='Renter Name')
-    age = fields.Char(string='Age')
+    age = fields.Char(string='Age', compute="_get_age")
     NID = fields.Char(string='NID')
-    # BOD = fields.Datetime(string="Date", default=lambda *a: datetime.now())
-    BOD = fields.Date(string="BOD")
+    BOD = fields.Date(string="BOD.")
     family_member = fields.Char(string='Family Member')
     address = fields.Text(string='Address')
     work_info = fields.Text(string='Work info.')
     phone = fields.Char(string='Phone')
     email = fields.Char(string='Email')
+
+    @api.onchange('BOD')
+    def _get_age(self):
+        today_date = datetime.date.today()
+        for age_ in self:
+            if age_.BOD:
+                BOD = fields.Datetime.to_datetime(age_.BOD).date()
+                total_age = str(int((today_date-BOD).days/365))
+                age_.age = total_age
+            else:
+                age_.age = 'Not Provide'
