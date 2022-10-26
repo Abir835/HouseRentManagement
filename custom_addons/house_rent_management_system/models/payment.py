7d4ec2f1e4e9e3ebd531b2d7134772c113a8ee6c
abir <abir.hasan@divine-it.bet>
2022-10-27 04:13:27 +0600

    APP<House Rent Management System> ADD:: add onchange property for names and details

--- custom_addons/house_rent_management_system/models/payment.py
+++ custom_addons/house_rent_management_system/models/payment.py
@@ -9,7 +9,19 @@ class Payment(models.Model):
 
     renter_id = fields.Many2one('property.renter', string='Renter Name')
     # date = fields.Datetime(string="Date", default=lambda *a: datetime.now())
-    date = fields.Date(string="Date")
+    # date = fields.Date(string="Date")
+    year = fields.Char(string='Year')
+    month = fields.Selection([('january', 'January'), ('february', 'February'), ('march', 'March'), ('april', 'April'),
+                              ('may', 'May'), ('june', 'June'), ('july', 'July'), ('august', 'August'),
+                              ('september', 'September'), ('october', 'October'), ('november', 'November'),
+                              ('december', 'December')])
+
+    renter_phone = fields.Char(string='Phone', readonly=True, compute='_get_phone')
+    renter_email = fields.Char(string='Email', readonly=True, compute='_get_email')
+    flat_property = fields.Char(string='Property Name', readonly=True, compute='_get_property_name')
+    flat_property_location = fields.Char(string='Property Location', readonly=True, compute='_get_property_location')
+    property_flat_name = fields.Char('Flat', redonly=True, compute='_get_flat_name_')
+    flat_price = fields.Char('Rent', redonly=True, compute='_get_flat_price')
 
     state = fields.Selection([
         ('pending', "Pending"),
@@ -26,5 +38,47 @@ class Payment(models.Model):
 
     def action_cancelled(self):
         self.state = 'cancelled'
+
+    @api.onchange('renter_id')
+    def _get_phone(self):
+        rent_id = self.renter_id
+        for rent_ in rent_id.renter_id:
+            if rent_.phone:
+                self.renter_phone = rent_.phone
+
+    @api.onchange('renter_id')
+    def _get_email(self):
+        rent_id = self.renter_id
+        for rent_email in rent_id.renter_id:
+            if rent_email.email:
+                self.renter_email = rent_email.email
+
+    @api.onchange('renter_id')
+    def _get_property_name(self):
+        flats_id = self.renter_id
+        for property_name in flats_id.flat_id.property_id:
+            if property_name.property_name:
+                self.flat_property = property_name.property_name
+
+    @api.onchange('renter_id')
+    def _get_property_location(self):
+        locations_id = self.renter_id
+        for loc_name in locations_id.flat_id.property_id.location_id:
+            if loc_name.address:
+                self.flat_property_location = loc_name.address
+
+    @api.onchange('renter_id')
+    def _get_flat_name_(self):
+        flat_name_id = self.renter_id
+        for flat_ in flat_name_id.flat_id:
+            if flat_.flat_name:
+                self.property_flat_name = flat_.flat_name
+
+    @api.onchange('renter_id')
+    def _get_flat_name(self):
+        price_id = self.renter_id
+        for price_ in price_id.flat_id:
+            if price_.price:
+                self.flat_price = price_.price
 
 
--- custom_addons/house_rent_management_system/models/renter.py
+++ custom_addons/house_rent_management_system/models/renter.py
@@ -11,6 +11,10 @@ class RenterDetails(models.Model):
     renter_id = fields.Many2one('renter.information', string='Renter Name')
     flat_id = fields.Many2one('property.flats', string='Flat No.')
     date = fields.Date(string="Date")
+    renter_phone = fields.Char(string='Phone', readonly=True, compute='_get_phone')
+    renter_email = fields.Char(string='Email', readonly=True, compute='_get_email')
+    flat_property = fields.Char(string='Property Name', readonly=True, compute='_get_property_name')
+    flat_property_location = fields.Char(string='Property Location', readonly=True, compute='_get_property_location')
     # date = fields.Datetime(string="Date", default=lambda *a: datetime.now())
 
     @api.onchange('flat_id')
@@ -20,3 +24,31 @@ class RenterDetails(models.Model):
             flat = self.env['property.renter'].search([('flat_id', '=', self.flat_id.id)])
             if flat:
                 raise ValidationError('Flat Already Rented')
+
+    @api.onchange('renter_id')
+    def _get_phone(self):
+        rent_id = self.renter_id
+        for rent_ in rent_id:
+            if rent_.phone:
+                self.renter_phone = rent_.phone
+
+    @api.onchange('renter_id')
+    def _get_email(self):
+        rent_id = self.renter_id
+        for rent_email in rent_id:
+            if rent_email.email:
+                self.renter_email = rent_email.email
+
+    @api.onchange('flat_id')
+    def _get_property_name(self):
+        flats_id = self.flat_id.property_id
+        for flat_name in flats_id:
+            if flat_name.property_name:
+                self.flat_property = flat_name.property_name
+
+    @api.onchange('flat_id')
+    def _get_property_location(self):
+        locations_id = self.flat_id.property_id.location_id
+        for loc_name in locations_id:
+            if loc_name.address:
+                self.flat_property_location = loc_name.address
