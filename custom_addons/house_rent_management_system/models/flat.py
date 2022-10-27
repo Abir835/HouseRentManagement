14fe952d4808fb45e70e65f82863f6686ed6ebbb
abir <abir.hasan@divine-it.bet>
2022-10-28 01:07:53 +0600

    APP<House Rent Management System> ADD:: add validation for flat add for flat total depends

--- custom_addons/house_rent_management_system/models/flat.py
+++ custom_addons/house_rent_management_system/models/flat.py
@@ -1,4 +1,5 @@
 from odoo import api, fields, models
+from odoo.exceptions import ValidationError
 
 
 class FlatDetails(models.Model):
@@ -9,3 +10,18 @@ class FlatDetails(models.Model):
     property_id = fields.Many2one('property.property', string='Property Name')
     flat_name = fields.Char(string='Flats Name')
     price = fields.Float(string='Rent Price')
+    flat_capacity = fields.Integer(string='Flat Capacity', readonly=True)
+    flat_created = fields.Integer(string='Flat Created', readonly=True)
+    # constrains from validation raise error
+
+    @api.onchange('property_id')
+    def total_flat(self):
+        rent_id = self.property_id
+        if rent_id:
+            total_count = rent_id.total_flat
+            self.flat_capacity = total_count
+            total_len = self.env['property.flats'].search_count([('property_id', '=', self.property_id.id)])
+            self.flat_created = total_len
+            if total_len >= total_count:
+                raise ValidationError('Over Property Flat Capacity for Add')
+
--- custom_addons/house_rent_management_system/models/property.py
+++ custom_addons/house_rent_management_system/models/property.py
@@ -9,17 +9,6 @@ class Property(models.Model):
     location_id = fields.Many2one('house.location', string='Location Info.')
     manager_id = fields.Many2one('manager.manager', string='Manager')
     property_name = fields.Char(string='Property Name')
-    # flat_ids = fields.Many2one('property.flats', 'property_id',)
-    # flat_count = fields.Integer(string="Total Flat", compute="_compute_total_flat", store=True)
+    total_flat = fields.Integer(string='Total Flat')
 
-    # @api.depends('flat_ids')
-    # def _compute_total_flat(self):
-    #     flat_group = self.env['property.flats'].read_group(fields=['property_id'], groupby=['property_id'])
-    #     for flat in flat_group:
-    #         property_id = flat.get('property_id')[0]
-    #         property_rec = self.browse(property_id)
-    #         property_rec.flat_count = flat['property_id_count']
-    #         self -= property_rec
-    #
-    #     self.flat_count = 0
 
