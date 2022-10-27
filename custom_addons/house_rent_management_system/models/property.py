6ea933b497151ff2bbd986eac442b57970390e1b
abir <abir.hasan@divine-it.bet>
2022-10-28 01:35:43 +0600

    APP<House Rent Management System> ADD:: manager validation add

--- custom_addons/house_rent_management_system/models/property.py
+++ custom_addons/house_rent_management_system/models/property.py
@@ -11,4 +11,13 @@ class Property(models.Model):
     property_name = fields.Char(string='Property Name')
     total_flat = fields.Integer(string='Total Flat')
 
+    @api.onchange('manager_id')
+    def is_manager_exist(self):
+        man_id = self.manager_id
+        if man_id:
+            manager_name = man_id.name
+            manager = self.env['property.property'].search([('manager_id', '=', self.manager_id.id)])
+            if manager:
+                raise ValueError(f"{manager_name}This manager is already occupied")
+
 
